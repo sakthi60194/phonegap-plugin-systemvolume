5e90f8bff1684be46622f01c00fcc3e14356e431
sakthi60194 <sakthipvmj@gmail.com>
2014-10-18 04:34:33 -0700

    eb commit

--- www/systemvolume.js
+++ www/systemvolume.js
@@ -3,6 +3,7 @@ var exec = require('cordova/exec');
 module.exports = {
 
     setSystemVolume: function(volume) {
+    	alert("hello");
         exec(null, null, "SystemVolume", "setSystemVolume", [volume]);
     },
 };
